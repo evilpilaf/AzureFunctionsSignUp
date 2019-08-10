72a9d0c27db1edb008a7e5d9fbfdf8025ccf8815
mauricio-dddelc <mauriciod73@gmail.com>
2019-08-10 10:06:27 +0200

    Add bootstrapping of domain library

---
+++ src/signup.core/Bootstrapper.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace SignUp.core
+{
+    public static class Bootstrapper
+    {
+        public static void Register(IServiceCollection serviceCollection)
+        {
+            serviceCollection.AddScoped<SignUpToCourseV1UseCase>();
+            serviceCollection.AddScoped<GetAllCoursesUseCase>();
+        }
+    }
+}
