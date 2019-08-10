aa0a99f3820f5d742b9976cad0f7c7ddb3c3de37
mauricio-dddelc <mauriciod73@gmail.com>
2019-08-10 10:05:39 +0200

    Add bootstrapping of persistence adapter

---
+++ Adapters/Persistence.Adapter/PersistenceAdapter.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+using SignUp.core.Ports;
+
+using SimpleInjector;
+
+namespace Persistence.Adapter
+{
+    public sealed class PersistenceAdapter
+    {
+        private readonly Container _container;
+
+        public PersistenceAdapter()
+        {
+            _container = new Container();
+            _container.Register<ICourseLoader, CourseLoaderCosmosDb>();
+        }
+
+        public void Register(IServiceCollection hostContainer)
+        {
+            hostContainer.AddScoped<ICourseLoader>(_ => _container.GetInstance<ICourseLoader>());
+        }
+    }
+}
