efffbf4bf92401898d94c7d8b70b42eb98e19a76
mauricio-dddelc <mauriciod73@gmail.com>
2019-08-10 19:57:23 +0200

    Implement CosmosDB method to get a single course

--- src/Adapters/Persistence.Adapter/CourseLoaderCosmosDb.cs
+++ src/Adapters/Persistence.Adapter/CourseLoaderCosmosDb.cs
@@ -6,6 +6,7 @@ using Persistence.Adapter.Mappers;
 using Serilog;
 
 using SignUp.core;
+using SignUp.core.Exceptions;
 using SignUp.core.Ports;
 using SignUp.core.ValueObjects;
 
@@ -13,7 +14,6 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using SignUp.core.Exceptions;
 
 namespace Persistence.Adapter
 {
@@ -30,7 +30,7 @@ namespace Persistence.Adapter
         public async Task<Result<IEnumerable<Course>>> GetAllCourses()
         {
             const string query = "SELECT * FROM c";
-            var container = _cosmosClient.GetContainer("CoursesChama", "Courses");
+            var container = GetContainer();
 
             var queryDefinition = new QueryDefinition(query);
 
@@ -56,9 +56,37 @@ namespace Persistence.Adapter
             }
         }
 
-        public Task<Result<Course>> GetCourse(Guid courseId)
+        public async Task<Result<Course>> GetCourse(Guid courseId)
         {
-            throw new NotImplementedException();
+            var container = GetContainer();
+            var query = $"SELECT * FROM c WHERE c.id = \'{courseId}\'";
+
+            var queryDefinition = new QueryDefinition(query);
+
+            try
+            {
+                _logger.Information("Attempting to retrieve a course.");
+                FeedIterator<CourseCosmosDto> feedIterator =
+                    container.GetItemQueryIterator<CourseCosmosDto>(queryDefinition,
+                        requestOptions: new QueryRequestOptions {MaxItemCount = 1});
+
+                var results = new List<CourseCosmosDto>();
+
+                var resultSet = await feedIterator.ReadNextAsync();
+                results.AddRange(resultSet.Resource);
+
+                if (results.Count != 1)
+                    return new CourseNotFoundException(courseId);
+                else
+                    return results.Single().ToEntity();
+            }
+            catch (Exception ex)
+            {
+                return new PersistenceException("", ex);
+            }
         }
+
+        private Container GetContainer()
+            => _cosmosClient.GetContainer("CoursesChama", "Courses");
     }
 }
